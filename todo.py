f158b79cb26ebd49af26f73cb89876184d4de51d
MoinulAx <mkhan@brooklynsteamcenter.org>
2023-03-14 10:52:35 -0400

    sd

--- todo.py
+++ todo.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template,request, redirect
 import pymysql.cursors
 import pymysql
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app= Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 
 connection= pymysql.connect(
@@ -22,8 +35,7 @@ cursor= connection.cursor()
  
 @app.route("/")
 def index(): 
-    cursor.execute("SELECT * FROM `Todo`")
-
+    cursor.execute("SELECT * FROM `Todo` ORDER BY `COMPLETED`")
     result= cursor.fetchall()
     
     print(result[0]['Description'])
@@ -35,15 +47,24 @@ def index():
     return render_template(
         'todotem.html.jinja',result=result)
     
-
 @app.route("/add",methods=['POST'])
+@auth.login_required
 def add():
     new_todo= request.form['new_todo']
     a.append(new_todo)        
     cursor.execute(f"INSERT INTO `Todo`(`Description`) VALUES ('{new_todo}') ")
+    return redirect("/")
+    
+@app.route("/complete", methods = ['POST'])
+def complete():
 
-    return redirect(('/'))
+    todo_id = request.form ['todo_id']
 
+    cursor = connection.cursor()
+    
+    cursor.execute(f"UPDATE `Todo` SET `Completed` = 1 WHERE `id` = {todo_id}" )
+
+    return redirect("/")
 
 
 
